b8e9d494c1fca87cc7a2c7ff58e8714490c6d946
Thomas George Wedgbury <P13229154@GH582-009.lec-admin.dmu.ac.uk>
2015-01-26 13:42:00 +0000

    uncommented all functions

--- maths.c
+++ maths.c
@@ -1,9 +1,9 @@
 include <stdio.h>
 
-//int add(x, y)
-//{
-//return x+y;
-//}
+int add(x, y)
+{
+return x+y;
+}
 
 int subtract(x, y)
 {
@@ -20,10 +20,10 @@ int divide(x, y)
 return x/y;
 }
 
-//int remainder(x,y)
-//{
-// return abs(x-y);
-//}
+int remainder(x,y)
+{
+return abs(x-y);
+}
 
 int main()
 {
