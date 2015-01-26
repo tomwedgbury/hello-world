629c868536a82544fc121d5c75f2eb2e7f381c11
Thomas George Wedgbury <P13229154@GH582-009.lec-admin.dmu.ac.uk>
2015-01-26 13:17:32 +0000

    Added maths.c with remainder function

---
+++ maths.c
@@ -0,0 +1,31 @@
+include <stdio.h>
+
+int add(x, y)
+{
+return x+y;
+}
+
+int subtract(x, y)
+{
+return x-y;
+}
+
+int multiply(x, y)
+{
+return x*y;
+}
+
+int divide(x, y)
+{
+return x/y;
+}
+
+int remainder(x,y)
+{
+ return abs(x-y);
+}
+
+int main()
+{
+return 0;
+}
