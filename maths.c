8872b4e981bb76b22ec5f4a2e9054d84e9ec104f
Thomas George Wedgbury <P13229154@GH582-009.lec-admin.dmu.ac.uk>
2015-01-26 13:13:42 +0000

    added maths.c

---
+++ maths.c
@@ -0,0 +1,26 @@
+include <stdio.h>
+
+int add(x, y)
+{
+return x+y;
+}
+
+int subtract(x, y)
+{
+return x-y;
+}
+
+int multiply(x, y)
+{
+return x*y;
+}
+
+int divide(x, y)
+{
+return x/y;
+}
+
+int main()
+{
+return 0;
+}
